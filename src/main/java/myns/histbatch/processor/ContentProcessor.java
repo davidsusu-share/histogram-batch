c8d6baac90327e342b055d85387ea4a0cbac22ea
Horváth Dávid <horvath@webarticum.hu>
2020-11-19 20:30:07 +0100

    Add functional annotation to ContentProcessor

--- src/main/java/myns/histbatch/processor/ContentProcessor.java
+++ src/main/java/myns/histbatch/processor/ContentProcessor.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 
+@FunctionalInterface
 public interface ContentProcessor {
 
     public void process(InputStream in, OutputStream out) throws IOException;
